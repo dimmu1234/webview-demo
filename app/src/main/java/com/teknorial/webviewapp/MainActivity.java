13ace8a13c474b1a55409068d208b193af63fe77
someone <someone@someplace.com>
2016-06-17 01:06:32 +0300

    some init msg

---
+++ app/src/main/java/com/teknorial/webviewapp/MainActivity.java
@@ -0,0 +1,136 @@
+package com.teknorial.webviewapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.net.URL;
+
+import static com.teknorial.webviewapp.R.id.button1;
+import static com.teknorial.webviewapp.R.id.button2;
+
+public class MainActivity extends ActionBarActivity {
+    private WebView view; //membuat variabel view agar bisa akses method onKeyDown
+    int pulsado = 0;
+    boolean web_view_not_visible = true;
+    TextView text;
+    Button myButton;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view = (WebView) this.findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.setWebViewClient(new MyBrowser());
+        view.loadUrl("http://www.3logicsoftware.com/spark/index.php"); //try js alert
+        view.setWebChromeClient(new WebChromeClient()); // adding js alert support
+
+        addButtonClickListner();
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void addButtonClickListner() {
+        Button btnNavigator = (Button) findViewById(button1);
+        btnNavigator.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg) {
+                view.loadUrl("javascript:myJSFunction()");
+            }
+        });
+
+
+
+        Button btnNavigator2 = (Button) findViewById(button2);
+        btnNavigator2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg) {
+                view.reload();
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.teknorial.webviewapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.teknorial.webviewapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //ketika disentuh tombol back
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && view.canGoBack()) {
+            view.goBack(); //method goback() dieksekusi untuk kembali pada halaman sebelumnya
+            return true;
+        }
+        // Jika tidak ada history (Halaman yang sebelumnya dibuka)
+        // maka akan keluar dari activity
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
